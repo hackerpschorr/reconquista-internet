94f8b7ddee9cf5d814e01fb0f959d6af94e819f4
Thilo Billerbeck <thilo.billerbeck@officerent.de>
2018-04-30 00:04:06 +0200

    implemented get_random_proxy, filtered out transparent proxies

--- tools/create_accounts/proxy_handler.py
+++ tools/create_accounts/proxy_handler.py
@@ -1,11 +1,12 @@
 import urllib.request
 import re
+import random
 import json
 import ast
 
 def proxy_list_fetch():
     with urllib.request.urlopen("https://raw.githubusercontent.com/fate0/proxylist/master/proxy.list") as response:
-        proxy_list_parse(response.read())
+        return proxy_list_parse(response.read())
 
 def proxy_list_parse(string):
     string = string.decode("utf-8")
@@ -13,5 +14,13 @@ def proxy_list_parse(string):
     proxiess = []
     for line in lines:
         if(line):
-            proxiess.append(json.loads(line))
+            decoded = json.loads(line)
+            if (decoded['anonymity'] != 'transparent'):
+                proxiess.append(decoded)
     return proxiess
+
+def get_random_proxy():
+    proxies = proxy_list_fetch()
+    randmax = len(proxies) - 1
+    proxy = proxies[random.randint(0,randmax)]
+    return proxy['host'] + ":" + str(proxy['port'])
